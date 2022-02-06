5c9788285056da1b8cac645ef8f50cadc5ddfb1f
reiyw <reiyw.setuve@gmail.com>
2022-02-06 17:58:23 +0900

    remove usused

--- src/main.rs
+++ src/main.rs
@@ -202,7 +202,6 @@ impl EventHandler for Handler {
                             (detail.duration.as_millis() as f64) * (100.0 / cmd.speed as f64);
                         let duration = duration as i64;
                         let mut wait = duration - cmd.start as i64;
-                        dbg!(&cmd);
                         if let Some(dur) = cmd.duration {
                             wait = cmp::min(wait, dur as i64)
                         }
