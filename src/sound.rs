2f7090fb186c0679b44931e7ebaec41ae3f8c4f6
Ryo Takahashi <reiyw.setuve@gmail.com>
2022-09-10 04:43:48 +0900

    wip

--- src/sound.rs
+++ src/sound.rs
@@ -6,10 +6,14 @@ use std::time::{Duration, SystemTime};
 
 use counter::Counter;
 use glob::glob;
+use notify::event::{CreateKind, ModifyKind, RenameMode};
+use notify::{Config, Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use once_cell::sync::OnceCell;
 use rand::rngs::StdRng;
 use rand::seq::IteratorRandom;
 use rand::SeedableRng;
+use tokio::runtime::Handle;
+use tokio::sync::mpsc;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Metadata {
@@ -17,6 +21,32 @@ struct Metadata {
     channel_count: u8,
     duration: Duration,
     updated_at: SystemTime,
+}
+
+impl Metadata {
+    fn load<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
+        let data = mp3_metadata::read_from_file(path.as_ref())?;
+        let freqs: Counter<_> = data.frames.iter().map(|f| f.sampling_freq).collect();
+        let sample_rate_hz = freqs.most_common()[0].0 as u32;
+        let channel_counts: Counter<_> = data
+            .frames
+            .iter()
+            .map(|f| match f.chan_type {
+                mp3_metadata::ChannelType::SingleChannel => 1,
+                // FIXME: I'm not sure this logic is correct.
+                _ => 2,
+            })
+            .collect();
+        let channel_count = channel_counts.most_common()[0].0;
+        let duration = data.duration;
+        let updated_at = fs::metadata(path.as_ref())?.modified()?;
+        Ok(Self {
+            sample_rate_hz,
+            channel_count,
+            duration,
+            updated_at,
+        })
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -31,7 +61,8 @@ pub struct Sound {
 }
 
 impl Sound {
-    fn from(path: impl AsRef<Path>) -> Self {
+    /// Initializes [`Sound`] without loading metadata.
+    fn new_unchecked<P: AsRef<Path>>(path: P) -> Self {
         Self {
             name: path.as_ref().file_stem().unwrap().to_string_lossy().into(),
             path: path.as_ref().into(),
@@ -39,68 +70,60 @@ impl Sound {
         }
     }
 
+    /// Initializes [`Sound`] with the metadata loaded.
+    ///
+    /// Use this method if the input file is unreliable as sound data.
+    fn new_checked<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
+        Ok(Self {
+            name: path.as_ref().file_stem().unwrap().to_string_lossy().into(),
+            path: path.as_ref().into(),
+            metadata: Metadata::load(path.as_ref())?.into(),
+        })
+    }
+
     pub fn sample_rate_hz(&self) -> u32 {
         self.metadata
-            .get_or_init(|| self.read_metadata())
+            .get_or_init(|| self.load_unchecked())
             .sample_rate_hz
     }
 
     pub fn channel_count(&self) -> u8 {
         self.metadata
-            .get_or_init(|| self.read_metadata())
+            .get_or_init(|| self.load_unchecked())
             .channel_count
     }
 
     pub fn duration(&self) -> Duration {
-        self.metadata.get_or_init(|| self.read_metadata()).duration
+        self.metadata.get_or_init(|| self.load_unchecked()).duration
     }
 
     pub fn updated_at(&self) -> SystemTime {
         self.metadata
-            .get_or_init(|| self.read_metadata())
+            .get_or_init(|| self.load_unchecked())
             .updated_at
     }
 
-    fn read_metadata(&self) -> Metadata {
-        let data = mp3_metadata::read_from_file(&self.path).unwrap();
-        let freqs: Counter<_> = data.frames.iter().map(|f| f.sampling_freq).collect();
-        let sample_rate_hz = freqs.most_common()[0].0 as u32;
-        let channel_counts: Counter<_> = data
-            .frames
-            .iter()
-            .map(|f| match f.chan_type {
-                mp3_metadata::ChannelType::SingleChannel => 1,
-                // FIXME: I'm not sure this logic is correct.
-                _ => 2,
-            })
-            .collect();
-        let channel_count = channel_counts.most_common()[0].0;
-        let duration = data.duration;
-        let updated_at = fs::metadata(&self.path).unwrap().modified().unwrap();
-        Metadata {
-            sample_rate_hz,
-            channel_count,
-            duration,
-            updated_at,
-        }
+    fn load_unchecked(&self) -> Metadata {
+        Metadata::load(&self.path)
+            .expect(&format!("Failed to load the metadata of {:?}", self.path))
     }
 }
 
-#[derive(Debug)]
-struct SoundStorage {
-    // Lowercased names to sounds.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SoundStorage {
+    /// Lowercased name to [`Sound`].
     sounds: BTreeMap<String, Sound>,
 
     dir: PathBuf,
 }
 
 impl SoundStorage {
-    pub fn load(dir: impl AsRef<Path>) -> Self {
+    pub fn load<P: AsRef<Path>>(dir: P) -> Self {
         let mut sounds = BTreeMap::new();
         for path in
             (glob(&format!("{}/**/*.mp3", dir.as_ref().to_string_lossy())).unwrap()).flatten()
         {
-            let sound = Sound::from(&path);
+            let sound = Sound::new_unchecked(&path);
             sounds.insert(sound.name.to_lowercase(), sound);
         }
         Self {
@@ -144,6 +167,83 @@ impl SoundStorage {
     pub fn len(&self) -> usize {
         self.sounds.len()
     }
+
+    // TODO: たぶん Mutex 使わないと一度 watch し始めたらあとから不変参照取れなくなる。
+    pub async fn watch(&mut self) {
+        let (tx, mut rx) = mpsc::channel(1);
+        let handle = Handle::current();
+        let mut watcher = RecommendedWatcher::new(
+            move |res| {
+                handle.block_on(tx.send(res)).unwrap();
+            },
+            Config::default(),
+        )
+        .unwrap();
+
+        watcher.watch(&self.dir, RecursiveMode::Recursive).unwrap();
+
+        while let Some(Ok(event)) = rx.recv().await {
+            // if event.paths[0].extension() != Some(OsStr::new("mp3")) {
+            if event.paths[0].ends_with(".mp3") {
+                continue;
+            }
+            match event {
+                Event {
+                    kind: EventKind::Create(CreateKind::File),
+                    paths,
+                    ..
+                }
+                | Event {
+                    kind: EventKind::Modify(ModifyKind::Data(_)),
+                    paths,
+                    ..
+                } => match Sound::new_checked(&paths[0]) {
+                    Ok(sound) => {
+                        self.add(sound);
+                    }
+                    #[allow(unused_variables)]
+                    Err(e) => {
+                        todo!();
+                    }
+                },
+                Event {
+                    kind: EventKind::Remove(_),
+                    paths,
+                    ..
+                } => {
+                    self.remove(paths[0].file_stem().unwrap().to_string_lossy());
+                }
+                Event {
+                    kind: EventKind::Modify(ModifyKind::Name(rename_mode)),
+                    paths,
+                    ..
+                } => match rename_mode {
+                    RenameMode::Any | RenameMode::Other => {
+                        if let Ok(sound) = Sound::new_checked(&paths[0]) {
+                            self.add(sound);
+                        } else {
+                            self.remove(paths[0].file_stem().unwrap().to_string_lossy());
+                        }
+                    }
+                    RenameMode::From => {
+                        self.remove(paths[0].file_stem().unwrap().to_string_lossy());
+                    }
+                    RenameMode::To => {
+                        if let Ok(sound) = Sound::new_checked(&paths[0]) {
+                            self.add(sound);
+                        }
+                    }
+                    RenameMode::Both => {
+                        self.remove(paths[0].file_stem().unwrap().to_string_lossy());
+                        if let Ok(sound) = Sound::new_checked(&paths[1]) {
+                            self.add(sound);
+                        }
+                    }
+                },
+                _ => {}
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -153,7 +253,7 @@ mod test {
     #[test]
     fn test_sound() {
         let sound_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/sound");
-        let sound = Sound::from(sound_dir.join("sainou.mp3"));
+        let sound = Sound::new_unchecked(sound_dir.join("sainou.mp3"));
         assert_eq!(sound.name, "sainou".to_string());
         assert_eq!(sound.path, sound_dir.join("sainou.mp3"));
         assert_eq!(sound.sample_rate_hz(), 44100);
@@ -197,5 +297,15 @@ mod test {
         assert_eq!(sims[0].1, storage.get("dadeisan").unwrap());
         assert_eq!(sims[1].1, storage.get("d").unwrap());
         assert_eq!(sims[2].1, storage.get("sainou").unwrap());
+    }
+
+    #[tokio::test]
+    async fn test_watch() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let mut storage = SoundStorage::load(&temp_dir);
+        // storage.watch().await;
+        tokio::spawn(storage.watch()).await.unwrap();
+        assert!(storage.get("sainou").is_some());
+        let sound_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/sound");
     }
 }
