e8aecf17fcf511c21de3ae5a167baee599d5e34a
Ryo Takahashi <reiyw.setuve@gmail.com>
2022-09-11 04:41:08 +0900

    basic commands

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeMap, HashMap},
+    collections::{HashMap},
     sync::Arc,
 };
 
@@ -20,7 +20,7 @@ use serenity::{
 /// Keeps track of channels where the bot joining.
 #[derive(Debug, Clone, Default)]
 struct ChannelManager {
-    channels: BTreeMap<GuildId, ChannelId>,
+    channels: HashMap<GuildId, ChannelId>,
 }
 
 impl ChannelManager {
@@ -35,7 +35,6 @@ impl ChannelManager {
 
 impl TypeMapKey for ChannelManager {
     type Value = Arc<RwLock<Self>>;
-    // type Value = Arc<Mutex<Self>>;
 }
 
 #[command]
@@ -91,17 +90,94 @@ async fn join_impl(ctx: &Context, msg: &Message) -> anyhow::Result<()> {
 #[command]
 #[only_in(guilds)]
 pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(e) = leave_impl(ctx, msg).await {
+        warn!("Error leaving the channel: {e:?}");
+    }
+    Ok(())
+}
+
+async fn leave_impl(ctx: &Context, msg: &Message) -> anyhow::Result<()> {
+    let guild = msg.guild(&ctx.cache).context("Guild's ID was not found")?;
+
+    let manager = songbird::get(ctx)
+        .await
+        .context("Songbird Voice client placed in at initialization.")?
+        .clone();
+
+    manager.remove(guild.id).await?;
+
+    let channel_manager = ctx
+        .data
+        .read()
+        .await
+        .get::<ChannelManager>()
+        .context("Could not get ChannelManager")?
+        .clone();
+    channel_manager.write().leave(&guild.id);
+
     Ok(())
 }
 
 #[command]
 #[only_in(guilds)]
 pub async fn mute(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(e) = mute_impl(ctx, msg).await {
+        warn!("Failed to mute: {e:?}");
+    }
+    Ok(())
+}
+
+async fn mute_impl(ctx: &Context, msg: &Message) -> anyhow::Result<()> {
+    let guild = msg.guild(&ctx.cache).context("Guild's ID was not found")?;
+
+    let manager = songbird::get(ctx)
+        .await
+        .context("Songbird Voice client placed in at initialization.")?
+        .clone();
+
+    let handler_lock = match manager.get(guild.id) {
+        Some(handler) => handler,
+        None => {
+            msg.reply(ctx, "Not in a voice channel").await?;
+            return Ok(());
+        }
+    };
+
+    let mut handler = handler_lock.lock().await;
+
+    handler.mute(true).await?;
+
     Ok(())
 }
 
 #[command]
 #[only_in(guilds)]
 pub async fn unmute(ctx: &Context, msg: &Message) -> CommandResult {
+    if let Err(e) = unmute_impl(ctx, msg).await {
+        warn!("Failed to unmute: {e:?}");
+    }
+    Ok(())
+}
+
+async fn unmute_impl(ctx: &Context, msg: &Message) -> anyhow::Result<()> {
+    let guild = msg.guild(&ctx.cache).context("Guild's ID was not found")?;
+
+    let manager = songbird::get(ctx)
+        .await
+        .context("Songbird Voice client placed in at initialization.")?
+        .clone();
+
+    let handler_lock = match manager.get(guild.id) {
+        Some(handler) => handler,
+        None => {
+            msg.reply(ctx, "Not in a voice channel").await?;
+            return Ok(());
+        }
+    };
+
+    let mut handler = handler_lock.lock().await;
+
+    handler.mute(false).await?;
+
     Ok(())
 }
